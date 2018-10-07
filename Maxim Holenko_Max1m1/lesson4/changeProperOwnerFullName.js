bc106dd04c842cd7ad6342eaa1af00e58611b96e
Maxim Holenko <maxim.ggolenko@gmail.com>
2018-10-07 18:06:33 +0300

    Update changeProperOwnerFullName.js

--- Maxim Holenko_Max1m1/lesson4/changeProperOwnerFullName.js
+++ Maxim Holenko_Max1m1/lesson4/changeProperOwnerFullName.js
@@ -3,7 +3,7 @@
 function Car(brand, fuelTank, ownerFullName) {
     this.brand = brand;
     this.fuelTank = fuelTank;
-    this.ownerFullName = (ownerFullName.split(' ').length !== 2) ? this.ownerFullName = "John Doe" : this.ownerFullName = ownerFullName;
+    this.ownerFullName = ownerFullName.split(' ').length !== 2 ? this.ownerFullName = "John Doe" : this.ownerFullName = ownerFullName;
 
     this.getFirstNamePart = function () {
         console.log("First name: " + this.ownerFullName.split(" ")[0]);
